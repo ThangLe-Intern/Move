35bdccc884107bf2a116838ed65ee0a063dd5534
LAPTOP-S02KLCA2\ledin <ledinhthang772@gmail.com>
2023-04-19 16:36:16 +0700

    task[Base] create skeleton ui

---
+++ app/src/main/java/com/madison/move/App.kt
@@ -0,0 +1,19 @@
+package com.madison.move
+
+import android.app.Application
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        init(this)
+    }
+
+    companion object {
+        var instance: App? = null
+            private set
+
+        private fun init(app: App) {
+            instance = app
+        }
+    }
+}
