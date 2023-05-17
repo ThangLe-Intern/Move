523b2f434701dbd6af6f4d9195e99bea883644a3
Vũ Dũng <103741274+nguyenvudung030121@users.noreply.github.com>
2023-05-17 15:00:55 +0700

    task[Menu UI] Fix Home UI

--- app/src/main/java/com/madison/move/ui/home/HomeFragment.kt
+++ app/src/main/java/com/madison/move/ui/home/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.alpha
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.CompositePageTransformer
