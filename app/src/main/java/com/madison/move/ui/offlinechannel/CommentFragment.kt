1877f059db2c26dfbf53da9ae2fe04e94659f081
LAPTOP-S02KLCA2\ledin <ledinhthang772@gmail.com>
2023-06-08 16:52:32 +0700

    call api guideline ss2

--- app/src/main/java/com/madison/move/ui/offlinechannel/CommentFragment.kt
+++ app/src/main/java/com/madison/move/ui/offlinechannel/CommentFragment.kt
@@ -13,6 +13,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
